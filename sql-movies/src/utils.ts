bae30df09251c21083f79ab39e9587379bbf53a4
Gvido Andis Čams <82043908+gvidocams@users.noreply.github.com>
2022-09-19 15:28:17 +0300

    Revert "Sql movies"

--- sql-movies/src/utils.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Source data may contain ' and " which may break your sql, escape them using this function.
- */
-export const escape = (source: string): string => {
-  return source.replace(/"/g, `'`).replace(/'/g, `''`);
-};
